2f4b2410e5d7935f2a4dbbd581a5821b9168d29d
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-20 16:38:35 -0300

    Service do Admin concluído

---
+++ api/src/admin/admin.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { hash } from 'bcryptjs';
+
+import { Admin } from './interfaces/admin.interface';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { UpdateAdminDto } from './dto/update-admin.dto';
+
+@Injectable()
+export class AdminService {
+  constructor(
+    @Inject('Admin') private readonly adminModel: Model<Admin>
+  ) { }
+
+  async findAll(): Promise<Admin[]> {
+    return await this.adminModel.find();
+  }
+
+  async findById(id: string): Promise<Admin> {
+    return await this.adminModel.findById(id);
+  }
+
+  async create(createAdminDto: CreateAdminDto): Promise<Admin> {
+    const password = await hash(createAdminDto.password, 10);
+    return await this.adminModel.create({ ...createAdminDto, password });
+  }
+
+  async update(id: string, updateAdminDto: UpdateAdminDto): Promise<Admin> {
+    return await this.adminModel.findByIdAndUpdate(id, updateAdminDto);
+  }
+
+  async updatePassword(id: string, password: string): Promise<Admin> {
+    password = await hash(password, 10);
+    return await this.adminModel.findByIdAndUpdate(id, { password });
+  }
+
+  async delete(id: string): Promise<boolean> {
+    return await this.adminModel.findByIdAndDelete(id).then(res => !!res);
+  }
+}
