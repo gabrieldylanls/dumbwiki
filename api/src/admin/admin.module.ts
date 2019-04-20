453cb0c6b72446912887d1757b746e9830476bd8
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-20 16:39:27 -0300

    Adicionando service ao módulo de Admin

--- api/src/admin/admin.module.ts
+++ api/src/admin/admin.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { AdminSchema } from './schemas/admin.schema';
+import { AdminService } from './admin.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Admin', schema: AdminSchema }])]
+  imports: [MongooseModule.forFeature([{ name: 'Admin', schema: AdminSchema }])],
+  providers: [AdminService]
 })
 export class AdminModule {}
