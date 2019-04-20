744cdc2be23d39198c8704f3cab0cfd2f81536e4
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-20 15:50:13 -0300

    Adicionando schema de Admin

---
+++ api/src/admin/schemas/admin.schema.ts
@@ -0,0 +1,38 @@
+import * as mongoose from 'mongoose';
+
+export const AdminSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    minlength: 5,
+    maxlength: 40,
+    required: true
+  },
+  userName: {
+    type: String,
+    minlength: 2,
+    maxlength: 10,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: true
+  },
+  photo: {
+    type: String,
+    default: 'http://mybroadband.co.za/news/wp-content/uploads/2017/04/Twitter-profile-picture.jpg',
+    required: true
+  },
+  birthDay: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
