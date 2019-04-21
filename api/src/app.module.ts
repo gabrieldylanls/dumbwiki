c3b2fd9b04d933811b1b5d7707a7634e4c60c002
Gabriel Dylan <gabriel.dylan.170@gmail.com>
2019-04-20 22:10:59 -0300

    AuthModule no AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/dwiki'),
-    AdminModule
+    AdminModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
